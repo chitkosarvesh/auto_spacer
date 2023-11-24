5409465c7c08b22223989aa2cb3a8a9374a05528
Sarvesh Chitko <chitkosarvesh@gmail.com>
2023-11-24 12:03:57 +0200

    better documentation example added

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+## [0.0.3]
+
+- Better documentation
+- example added
+
+## [0.0.2]
+
+- Tests added
+
+## [0.0.1]
+
+- Main code for the project
--- example/main.dart
+++ example/main.dart
@@ -1,14 +1,3 @@
-# auto_spacer
-    
-[![pub package](https://img.shields.io/pub/v/auto_spacer.svg)](https://pub.dartlang.org/packages/auto_spacer)
-[![style: very good analysis](https://img.shields.io/badge/style-very_good_analysis-B22C89.svg)](https://pub.dev/packages/very_good_analysis)
-![Codecov](https://img.shields.io/codecov/c/github/chitkosarvesh/auto_spacer)
-
-A simple Flutter package to insert a spacer between a list of widgets
-
-## Usage
-
-```dart
 import 'package:auto_spacer/auto_spacer.dart';
 import 'package:flutter/material.dart';
 
@@ -67,4 +56,3 @@ class ExampleScreen extends StatelessWidget {
     );
   }
 }
-```
